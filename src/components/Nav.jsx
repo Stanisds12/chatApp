457ba59c856aab15530dc4deec27f26fe2255d57
Stanislau Ebo da Silva <acido2009@hotmail.com.br>
2021-07-05 05:58:47 -0400

    github Link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href={"/"}>Navbar</a>
+                <a className="navbar-brand" href={"https://github.com/Stanisds12"}>Stani DEV</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
